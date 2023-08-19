5f0d2e976e9ad47b4a718c70a7cbb43ade36b644
qzonic <lazzytoo@mail.ru>
2023-08-19 16:02:21 +0300

    some fix voice_answer.py (add finally block)

--- handlers/voice_answer.py
+++ handlers/voice_answer.py
@@ -9,13 +9,14 @@ async def recognise_and_send_answer(message: types.Message):
     if message.content_type == types.ContentType.VOICE:
         voice_id = message.voice.file_unique_id
         file_path = f'voices/{voice_id}.ogg'
+        audio_converter = AudioConverter(file_path)
         try:
-            audio_converter = AudioConverter(file_path)
             text = audio_converter.convert()
             await message.answer(OPENAI_ANSWER.format(text))
             response = audio_converter.openai_response(text)
             await message.answer(response)
-            audio_converter.delete()
         except UnknownValueError:
             await message.answer(RECOGNITION_ERROR)
+        finally:
+            audio_converter.delete()
 
